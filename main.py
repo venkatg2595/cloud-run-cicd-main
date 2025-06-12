d6b9a979f800d51512e6103c5050e7a81790fc73
Venkateswarlu Gude <venkateswarlu.gude@fractal.ai>
2025-06-12 12:31:46 +0530

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ version = os.environ.get("VERSION", "dev")
 
 @app.route("/")
 def hello():
-    return f"Hello from Cloud Run Welcomes you! Version: {version}"
+    return f"this is venkateswarlu and Cloud Run Welcomes you! Version: {version}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
